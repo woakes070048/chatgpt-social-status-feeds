ce548c2391c5476b36af8a6b5199fbe03087a37f
Nikolai X. Shadeauxs <services@vontainment.com>
2023-03-24 20:50:50 -0400

    Update config.php

--- public/config.php
+++ public/config.php
@@ -1,5 +1,5 @@
 <?php
-define('API_KEY', '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');
+define('API_KEY', 'fghfghfghfghfgfg');
 define('API_ENDPOINT', 'https://api.openai.com/v1/chat/completions');
 define('MODEL', 'gpt-3.5-turbo');
 define('TEMPERATURE', 0.7);
