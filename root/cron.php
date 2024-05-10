8bcf1f685a1426f27a10cb15d53b175973c69f8c
Nikolai X. Shadeauxs <services@vontainment.com>
2024-05-10 02:41:34 -0400

    Update and rename root/public/cron.php to root/cron.php

---
+++ root/cron.php
@@ -0,0 +1,96 @@
+<?php
+/*
+ * Project: ChatGPT API
+ * Author: Vontainment
+ * URL: https://vontainment.com
+ * File: cron.php
+ * Description: ChatGPT API Status Generator
+ */
+
+// 0 9,12,18 * * * php /path/to/cron/cron.php job_type=run_status
+// 0 0 1 * * php /path/to/cron.php job_type=reset_usage
+
+require_once './config.php';
+require_once '/lib/common-lib.php';
+require_once '/lib/status-lib.php';
+
+// Checking for command line arguments for job type
+$jobType = $argv[1] ?? 'run_status'; // Default to 'run_status' if no argument provided
+
+if ($jobType == 'reset_usage') {
+    // Reset used API calls for all users
+    resetApiUsage();
+} elseif ($jobType == 'run_status') {
+    // Run status update jobs
+    runStatusUpdateJobs();
+}
+
+function runStatusUpdateJobs() {
+    // Define times for cron jobs
+    $cronTimes = [
+        1 => ['12:00'],
+        2 => ['09:00', '12:00'],
+        3 => ['09:00', '12:00', '18:00']
+    ];
+
+    // Current time
+    $currentHour = date('H:00'); // Gets the current hour in HH:00 format
+    $currentTimeSlot = date('Y-m-d H:i:s');
+
+    // Fetch all accounts
+    $accounts = getAllAccounts();
+
+    foreach ($accounts as $account) {
+        $accountOwner = $account->username;
+        $accountName = $account->account;
+        $key = $account->key;
+
+        $acctInfo = getAcctInfo($accountOwner, $accountName);
+        $userInfo = getUserInfo($accountOwner);
+
+        if (in_array($currentHour, $cronTimes[$acctInfo->cron])) {
+            if (!hasStatusBeenPosted($accountName, $accountOwner, $currentTimeSlot)) {
+                if ($userInfo && $userInfo->used_api_calls < $userInfo->max_api_calls) {
+                    $userInfo->used_api_calls += 1;
+                    // Update user info in the database
+
+                    $prompt = $acctInfo->prompt;
+                    $link = $acctInfo->link;
+                    $hashtags = $acctInfo->hashtags;
+                    generateStatus($accountName, $accountOwner, $key, $prompt, $link, $hashtags);
+                }
+            }
+        }
+    }
+}
+
+function resetApiUsage() {
+    $db = new Database();
+    $db->query("UPDATE users SET used_api_calls = 0");
+    $db->execute();
+}
+
+function getAllAccounts() {
+    $db = new Database();
+    $db->query("SELECT * FROM accounts");
+    return $db->resultSet();
+}
+
+function hasStatusBeenPosted($accountName, $accountOwner, $currentTimeSlot) {
+    $db = new Database();
+    // Calculate time window
+    $startTime = date('Y-m-d H:i:s', strtotime($currentTimeSlot . ' -15 minutes'));
+    $endTime = date('Y-m-d H:i:s', strtotime($currentTimeSlot . ' +15 minutes'));
+
+    // Query to check for status within the +/- 15-minute window of the current time slot
+    $db->query("SELECT COUNT(*) as count FROM status_updates WHERE username = :username AND account = :account AND created_at BETWEEN :startTime AND :endTime");
+    $db->bind(':username', $accountOwner);
+    $db->bind(':account', $accountName);
+    $db->bind(':startTime', $startTime);
+    $db->bind(':endTime', $endTime);
+    $result = $db->single();
+
+    // Return true if a status has been posted in the time window, false otherwise
+    return $result->count > 0;
+}
+?>
